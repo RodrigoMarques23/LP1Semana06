5aed12a9a337371e95064484586ce721a7708327
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-04-20 19:31:56 +0100

    Constructor add to Foe

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -2,6 +2,18 @@ namespace GameSix
 {
     public class Foe
     {
-        
+        private string name;//var name
+        private float health;//var health
+        private float shield;//var shield
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        public Foe(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
