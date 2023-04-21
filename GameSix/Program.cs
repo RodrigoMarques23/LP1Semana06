50dde25613d7ed443b46014a88382d0719c98567
RodrigoMarques23 <45172954+RodrigoMarques23@users.noreply.github.com>
2023-04-21 01:08:47 +0100

    Ex 05 Solved

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,7 +6,21 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nº de inimigos?");
+            int x = Int32.Parse(Console.ReadLine());
+            Foe[] enemies = new Foe[x];
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Nome de inimigo?");
+                string s = Console.ReadLine();
+                enemies[i] = new Foe(s);
+            }
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Console.WriteLine($" Nome do inimigo {i+1} : {enemies[i].GetName()}");
+            }
         }
     }
 }
